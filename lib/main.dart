b61b71da9c8f8d9d5084093a812233f8bd03bffc
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-05-15 20:22:20 -0700

    End of lesson 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/image_composition.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart' hide Image;
 
 void main() {
@@ -12,12 +14,22 @@ class ChickenGame extends FlameGame {
   double chickenScaleFactor = 3.0;
 
   late SpriteAnimationComponent chicken;
+  late SpriteComponent background;
+  late final JoystickComponent joystick;
+  bool chickenFlipped = false;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
+    Flame.device.setLandscape();
+
     print('2. load the assets for the game');
+
+    background = SpriteComponent()
+      ..sprite = await loadSprite('background.png')
+      ..size = size;
+    add(background);
 
     Image chickenImage = await images.load('chicken.png');
 
@@ -37,6 +49,25 @@ class ChickenGame extends FlameGame {
 
     // add sprite to UI
     add(chicken);
+
+    final joystickKnobPaint = BasicPalette.blue.withAlpha(200).paint();
+    final joystickBackgroundPaint = BasicPalette.blue.withAlpha(100).paint();
+    joystick = JoystickComponent(
+      knob: CircleComponent(
+        radius: 30,
+        paint: joystickKnobPaint,
+      ),
+      background: CircleComponent(
+        radius: 60,
+        paint: joystickBackgroundPaint,
+      ),
+      margin: const EdgeInsets.only(
+        left: 40,
+        bottom: 40,
+      ),
+    );
+
+    add(joystick);
   }
 
   @override
@@ -46,5 +77,16 @@ class ChickenGame extends FlameGame {
     // the following is just a demonstration
     // chicken.y += 1;
     // chicken.x -= 1;
+    chicken.position.add(joystick.relativeDelta * 300 * dt);
+
+    if (joystick.relativeDelta[0] < 0 && chickenFlipped) {
+      chickenFlipped = false;
+      chicken.flipHorizontallyAroundCenter();
+    }
+    if (joystick.relativeDelta[0] > 0 && !chickenFlipped) {
+      chickenFlipped = true;
+      chicken.flipHorizontallyAroundCenter();
+    }
+    //print(joystick.relativeDelta);
   }
 }
